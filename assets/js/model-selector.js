79e77fa8c3daf7898a6cfaf3fcf97fd1bbaa19bd
PimVanLommel <mjgrashoff@aol.com>
2020-12-21 11:23:05 +0100

    removed search button

--- assets/js/model-selector.js
+++ assets/js/model-selector.js
@@ -1,6 +1,5 @@
 var modelsJSON = null;
 var selectorValues = {
-    "started": false,
     "language": "",
     "training": "",
     "gpu": "",
@@ -15,66 +14,43 @@ $.getJSON("assets/models.json", function (json) {
         return parseFloat(b.glue) - parseFloat(a.glue);
     });
 
-    if(localStorage.getItem("selectorValues") !== null) {
-        selectorValues = JSON.parse(localStorage.getItem("selectorValues"));
-        checkSelectors();
-    }
+    fillTable();
 });
 
 $('#language').on('change', function () {
     console.log('Language: ', $(this).val());
-    selectorValues.started = true;
     selectorValues.language = $(this).val();
-    localStorage.setItem('selectorValues', JSON.stringify(selectorValues));
-    checkSelectors();
+    fillTable();
 });
 
 $('#training').on('change', function () {
     console.log('Training: ', $(this).val());
-    selectorValues.started = true;
     selectorValues.training = $(this).val();
-    localStorage.setItem('selectorValues', JSON.stringify(selectorValues));
-    checkSelectors();
+    fillTable();
 });
 
 $('#gpu').on('change', function () {
     console.log('GPU: ', $(this).val());
-    selectorValues.started = true;
     selectorValues.gpu = $(this).val();
-    localStorage.setItem('selectorValues', JSON.stringify(selectorValues));
-    checkSelectors();
+    fillTable();
 });
 
 $('#textLength').on('change', function () {
     console.log('Text Length: ', $(this).val());
-    selectorValues.started = true;
     selectorValues.textLength = $(this).val();
-    localStorage.setItem('selectorValues', JSON.stringify(selectorValues));
-    checkSelectors();
+    fillTable();
 });
 
 $('#amount').on('change', function () {
     console.log('Amount: ', $(this).val());
-    selectorValues.started = true;
     selectorValues.amount = $(this).val();
-    localStorage.setItem('selectorValues', JSON.stringify(selectorValues));
-    checkSelectors();
+    fillTable();
 });
 
 $('#search').on('click', function () {
     $('#table-container').removeClass('hide');
     fillTable();
 });
-
-function checkSelectors() {
-    if(selectorValues.language !== "" &&
-        selectorValues.training !== "" &&
-        selectorValues.gpu !== "" &&
-        selectorValues.amount !== "" &&
-        selectorValues.textLength !== "") {
-        $('#search').removeClass('disabled');
-    }
-}
 
 function fillTable() {
     console.log(selectorValues);
@@ -90,24 +66,22 @@ function fillTable() {
         var amount = specs.amount;
         var textLength = specs.textLength;
         var languages = specs.languages;
-        var check = checkLanguage(languages, selectorValues.training);
-        var language = check[0];
-        var iterator = check[1];
+        var check = selectorValues.language !== '' ? checkLanguage(languages, selectorValues.training) : true;
+        var language = selectorValues.language !== '' ? check[0] : 'english';
+        var iterator = selectorValues.language !== '' ? check[1] : 0;
 
-        if(selectorValues.started === true &&
-            (training === selectorValues.training) &&
-            (gpu <= selectorValues.gpu) &&
-            (amount <= selectorValues.amount) &&
-            (textLength >= selectorValues.textLength) && check !== false) {
+        if ((selectorValues.training === '' || training === selectorValues.training) &&
+            (selectorValues.gpu === '' || gpu <= selectorValues.gpu) &&
+            (selectorValues.amount === '' || amount <= selectorValues.amount) &&
+            (selectorValues.textLength === '' || textLength >= selectorValues.textLength) &&
+            check !== false) {
             lmFound = true;
             lmCount++;
             
-            console.log(lmCount);
-
             if (lmCount <= 3) {
                 html += '' +
                     '<tr>' +
-                    '<td style="width: 90px; max-width: 90px;">' + modelsJSON[i].name + (selectorValues.training === 'trained' ? '-' + language : '') + '</td>' +
+                    '<td style="width: 90px; max-width: 90px;">' + modelsJSON[i].name + (selectorValues.training === 'trained' || selectorValues.training === '' ? '-' + language : '') + '</td>' +
                     '<td style="width: 80px; max-width: 80px;">' + modelsJSON[i].glue + '</td>' +
                     '<td style="width: 110px; max-width: 110px;"><a href="' + modelsJSON[i].github + '" target="_blank">' + modelsJSON[i].github + '</a></td>' +
                     '<td style="width: 250px; max-width: 250px;">' + modelsJSON[i].description + '</td>' +
